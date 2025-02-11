e00e1afc3ee08ec8e1f2b7365255c6c91172b7f7
mktcode <kontakt@markus-kottlaender.de>
2025-02-11 16:46:36 +0100

    fix: update Hero component background image

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="grow relative h-96 md:h-[40rem] bg-cover bg-left-bottom" style="background-image: url('/img/header2.jpg');">
+  <section class="grow relative h-96 md:h-[40rem] bg-cover bg-left-bottom" style="background-image: url('/img/header6.jpg');">
     <div class="bg-white h-96 lg:h-64 2xl:h-44 3xl:h-32" />
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
       <path fill="#ffffff" fill-opacity="1" d="M0,160L48,165.3C96,171,192,181,288,197.3C384,213,480,235,576,213.3C672,192,768,128,864,96C960,64,1056,64,1152,69.3C1248,75,1344,85,1392,90.7L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
