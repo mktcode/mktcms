ac9c9900756b59714f55659ee12aad819d4b8ede
mktcode <kontakt@markus-kottlaender.de>
2025-02-20 19:43:00 +0100

    fix: correct ability check for website management to match manager's Google ID

--- server/utils/abilities.ts
+++ server/utils/abilities.ts
@@ -3,5 +3,5 @@ import { User } from "#auth-utils"
 export const manageWebsite = defineAbility((user: User) => {
   const managerGoogleId = process.env.MANAGER_GOOGLE_ID
 
-  return user.googleId !== managerGoogleId
+  return user.googleId === managerGoogleId
 })
