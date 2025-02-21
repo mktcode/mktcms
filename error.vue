14050b971fe9237496c69100f2f8f08146882f2d
mktcode <kontakt@markus-kottlaender.de>
2025-02-21 12:50:24 +0100

    feat: add custom error page with error handling and redirect functionality

---
+++ error.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps({
+  error: Object as () => NuxtError
+})
+
+const handleError = () => clearError({ redirect: '/' })
+</script>
+
+<template>
+  <NuxtLayout name="default">
+    <div class="flex flex-col items-center justify-center h-screen space-y-4">
+      <img src="/img/logo.png" alt="Error" />
+      <h2 class="text-2xl text-center">
+        Ups! Ein Fehler ist aufgetreten.
+      </h2>
+      <button @click="handleError" class="button mt-4">
+        Zurück zur Startseite
+      </button>
+    </div>
+  </NuxtLayout>
+</template>
