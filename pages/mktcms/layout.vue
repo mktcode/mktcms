2c786b7b08411aa8e735e8054dec2354067249d9
mktcode <kontakt@markus-kottlaender.de>
2025-02-24 15:11:34 +0100

    feat: create Theme component for customizable primary colors

--- pages/mktcms/layout.vue
+++ pages/mktcms/layout.vue
@@ -15,44 +15,12 @@ definePageMeta({
   },
 })
 
-const theme = await useTheme()
-const primaryColor = ref(theme.primaryColor)
-const primaryColorHover = ref(theme.primaryColorHover)
-
-const save = async () => {
-  await useFetch('/api/theme/save', {
-    method: 'POST',
-    body: {
-      primaryColor: primaryColor.value,
-      primaryColorHover: primaryColorHover.value,
-    },
-  })
-}
-
 const pages = await $fetch('/api/pages/list', { method: 'POST' })
 </script>
 
 <template>
   <div>
-    <h1 class="text-3xl font-bold mb-4">
-      Farben
-    </h1>
-
-    <form class="grid grid-cols-1 gap-4 sm:grid-cols-2">
-      <div>
-        <label for="primaryColor">Primärfarbe</label>
-        <input id="primaryColor" type="color" v-model="primaryColor" />
-      </div>
-      <div>
-        <label for="primaryColorHover">Primärfarbe (hover)</label>
-        <input id="primaryColorHover" type="color" v-model="primaryColorHover" />
-      </div>
-    </form>
-    <div class="mt-4 flex justify-end">
-      <button type="submit" class="button" @click="save">
-        Speichern
-      </button>
-    </div>
+    <MktcmsLayoutTheme />
 
     <template v-for="page in pages">
       <h1 class="text-3xl font-bold mb-4 mt-10">
