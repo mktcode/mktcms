b652cbb5beaf266467339957b9914104c48d4399
mktcode <kontakt@markus-kottlaender.de>
2025-03-07 10:42:58 +0100

    feat: add Order and Vcard components for payment confirmation and marketing display

---
+++ components/planner/Order.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="mt-24 flex gap-12">
+    <div>
+      <h1 class="text-3xl font-bold text-gray-900 mb-4">
+        Bestätigen und Bezahlen
+      </h1>
+  
+      <p class="text-lg text-gray-700 mb-8">
+        Hier sind nochmal die einzelnen Posten aufgelistet. Wenn alles passt, kannst du hier bezahlen. Du kannst natürlich monatlich kündigen.
+      </p>
+    </div>
+    <div>
+      <table class="w-full prose prose-sky min-w-lg">
+        <thead>
+          <tr>
+            <th class="text-left">Posten</th>
+            <th class="text-right">Preis pro Monat</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="text-left">Domain und E-Mail</td>
+            <td class="text-right">5 €</td>
+          </tr>
+          <tr>
+            <td class="text-left">
+              Hauptseite
+            </td>
+            <td class="text-right">12 €</td>
+          </tr>
+          <tr>
+            <td class="text-left">
+              Angebot: Software zum halben Preis
+            </td>
+            <td class="text-right">6 €</td>
+          </tr>
+          <tr>
+            <td class="text-left">KI-Assistent</td>
+            <td class="text-right">25 €</td>
+          </tr>
+          <tr>
+            <td class="text-left">Änderungshotline</td>
+            <td class="text-right">34 €</td>
+          </tr>
+          <tr>
+            <td class="text-left font-bold text-2xl">
+              Summe
+            </td>
+            <td class="text-right font-bold text-2xl">
+              132 €
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <button class="button w-full mt-4">
+        Bestätigen und Bezahlen
+      </button>
+    </div>
+  </div>
+</template>
---
+++ components/planner/Vcard.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { PhoneIcon } from '@heroicons/vue/24/outline'
+</script>
+
+<template>
+  <div class="flex">
+    <div class="shadow-xl max-w-md bg-white -rotate-3 flex gap-4 aspect-8/5 w-full z-20 hover:scale-105 hover:rotate-0 hover:shadow-2xl">
+      <div class="p-6">
+        <div class="flex items-center gap-4">
+          <div>
+            <img src="~/assets/img/mktcms.png" alt="Logo" class="rounded-full" :style="{ width: `100px` }" />
+          </div>
+          <div>
+            <div class="font-bold">
+              Marketing CMS
+            </div>
+            <div>
+              Das Marketing-Tool für KMU
+            </div>
+          </div>
+        </div>
+        <div class="mt-8 text-3xl font-bold">
+          Dein Slogan
+        </div>
+        <div class="mt-1 text-lg text-gray-700">
+          Deine Beschreibung
+        </div>
+        <div class="mt-8 flex gap-2 items-center">
+          <PhoneIcon class="size-5 text-gray-400" />
+          <div class="text-xl text-gray-600">
+            0123 456789
+          </div>
+        </div>
+      </div>
+      <img src="~/assets/img/default-header.jpg" alt="Header" class="w-36 aspect-9/16 object-cover" />
+    </div>
+    <div class="shadow-xl max-w-md bg-white rotate-3 flex gap-4 aspect-8/5 w-full z-10 -ml-6 hover:z-30 hover:scale-105 hover:rotate-0 hover:shadow-2xl">
+      <div class="p-16 text-center flex flex-col items-center justify-center gap-4 w-full">
+        <img src="~/assets/img/mktcms.png" alt="Logo" class="rounded-full" :style="{ width: `120px` }" />
+        <div class="text-3xl font-bold">
+          Ein Programmierer zum anfassen!
+        </div>
+        <div class="text-lg text-gray-700">
+          Wir programmieren Ihre Website und vieles mehr. Einfach und schnell.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
