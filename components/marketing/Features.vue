97685131fe65298a034ba7643ffc9949f90d9a2c
Markus Kottländer <kontakt@markus-kottlaender.de>
2025-03-13 21:34:18 +0100

    feat: adjusted marketing text

--- components/marketing/Features.vue
+++ components/marketing/Features.vue
@@ -3,7 +3,7 @@ const features = [
   {
     name: 'Werde gefunden.',
     description:
-      'Verwalte dein Google Unternehmensprofil und deine Website in einer App und konzentriere dich dabei auf deine Angebote und Botchaften.',
+      'Verwalte dein Google Unternehmensprofil und deine Website in einer App und konzentriere dich dabei auf deine Angebote und Botschaften.',
     icon: 'i-heroicons-map-pin',
   },
   {
@@ -25,13 +25,13 @@ const features = [
       <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
         <div class="lg:pt-4 lg:pr-8">
           <div class="lg:max-w-lg">
-            <h2 class="text-base/7 font-semibold text-sky-600">Alles was du JETZT brauchst</h2>
-            <p class="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
-              Minimal und effektiv
+            <h2 class="text-base/7 font-semibold text-sky-600">Für Einsteiger</h2>
+            <p class="text-3xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-4xl">
+              Werbung und Buchhaltung in einer App
             </p>
             <p class="mt-6 text-lg/8 text-gray-600">
-              Das CMS hilft Kleinunternehmern und Gründern, die mit Technik nichts am Hut haben, aber Werbemittel und eine einfache Buchhaltung brauchen.
-              Schnell startklar, leicht zu bedienen und Sie haben den Kopf frei fürs Wesentliche.
+              Mkt's CMS hilft Einzelunternehmern, die nicht gerne am Computer sitzen und klare, übersichtliche Lösungen schätzen.
+              Sofort startklar, sehr leicht zu bedienen und Sie haben den Kopf frei fürs Wesentliche.
             </p>
             <dl class="flex  flex-col mt-10 max-w-xl gap-4 text-base/7 text-gray-600 lg:max-w-none">
               <div v-for="feature in features" :key="feature.name" class="relative pl-9">
