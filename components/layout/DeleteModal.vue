fadc0104b61aa6b416e3c4f4ea7c2fa152e96183
mktcode <kontakt@markus-kottlaender.de>
2025-03-13 09:16:04 +0100

    feat: add DeleteModal component for confirming data deletion

---
+++ components/layout/DeleteModal.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+
+const emit = defineEmits<{ close: [boolean] }>()
+</script>
+
+<template>
+  <UModal
+    :close="{ onClick: () => emit('close', false) }"
+    :title="`Löschen von ${title}`"
+    description="Möchten Sie diesen Datensatz wirklich löschen? Diese Aktion kann nicht rückgängig gemacht werden."
+  >
+    <template #footer>
+      <div class="w-full flex justify-between gap-2">
+        <UButton color="neutral" label="Abbrechen" @click="emit('close', false)" />
+        <UButton color="error" variant="link" label="Wirklich löschen" icon="i-heroicons-trash" @click="emit('close', true)" />
+      </div>
+    </template>
+  </UModal>
+</template>
