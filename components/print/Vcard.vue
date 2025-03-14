0a3047b458d5f93ebe1f6125bcfa2f7752737c31
mktcode <kontakt@markus-kottlaender.de>
2025-03-14 16:02:42 +0100

    feat: add Vcard component

---
+++ components/print/Vcard.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+defineProps<{
+  logoWidth: number
+  title: string
+  subtitle: string
+  street: string
+  zip: string
+  city: string
+  email: string
+  phone: string
+  slogan: string
+  description: string
+}>()
+</script>
+
+<template>
+  <div class="@container bg-white flex aspect-8/5 w-full overflow-hidden">
+    <div class="p-[5cqw] flex flex-col gap-4 w-full">
+      <div class="flex items-center gap-[3cqw]">
+        <div>
+          <img src="~/assets/img/mktcms.png" alt="Logo" class="rounded-full w-[15cqw]" />
+        </div>
+        <div>
+          <div class="font-bold text-[3cqw] leading-none">
+            {{ title }}
+          </div>
+          <div class="text-[3cqw]">
+            {{ subtitle }}
+          </div>
+        </div>
+      </div>
+      <div class="text-[5cqw] font-bold">
+        {{ slogan }}
+      </div>
+      <div class="flex flex-col gap-[1cqw]">
+        <div class="flex gap-[1cqw] items-start" v-if="street && zip && city">
+          <UIcon name="i-heroicons-building-office-2" class="size-[3cqw] text-gray-300 shrink-0" />
+          <div class="text-[3cqw] text-gray-600 leading-tight">
+            {{ street }},<br />{{ zip }} {{ city }}
+          </div>
+        </div>
+        <div class="flex gap-[1cqw] items-center" v-if="phone">
+          <UIcon name="i-heroicons-phone" class="size-[3cqw] text-gray-300 shrink-0" />
+          <div class="text-[3cqw] text-gray-600">
+            {{ phone }}
+          </div>
+        </div>
+        <div class="flex gap-[1cqw] items-center" v-if="email">
+          <UIcon name="i-heroicons-envelope" class="size-[3cqw] text-gray-300 shrink-0" />
+          <div class="text-[3cqw] text-gray-600">
+            {{ email }}
+          </div>
+        </div>
+      </div>
+    </div>
+    <img src="~/assets/img/default-header.jpg" alt="Header" class="w-36 aspect-9/16 object-cover" />
+  </div>
+</template>
--- components/vcards/Form.vue
+++ components/vcards/Form.vue
@@ -11,7 +11,7 @@ type Schema = z.output<typeof schema>
 
 const state = reactive<Partial<Schema>>({
   id: props.vcard?.id,
-  title: props.vcard?.title,
+  title: props.vcard?.title ?? '',
   street: props.vcard?.street ?? '',
   zip: props.vcard?.zip ?? '',
   city: props.vcard?.city ?? '',
@@ -36,36 +36,56 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
 </script>
 
 <template>
-  <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
-    <UFormField label="Titel" name="title" size="xl">
-      <UInput v-model="state.title" class="w-full" />
-    </UFormField>
-
-    <UFormField label="Straße" name="street" size="xl">
-      <UInput v-model="state.street" class="w-full" />
-    </UFormField>
-
-    <div class="flex gap-4">
-      <UFormField label="PLZ" name="zip" size="xl">
-        <UInput v-model="state.zip" class="w-full" />
-      </UFormField>
-  
-      <UFormField label="Ort" name="city" size="xl" class="grow">
-        <UInput v-model="state.city" class="w-full" />
-      </UFormField>
+  <div class="flex gap-8">
+    <div>
+      <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
+        <UFormField label="Titel" name="title" size="xl">
+          <UInput v-model="state.title" class="w-full" />
+        </UFormField>
+    
+        <UFormField label="Straße" name="street" size="xl">
+          <UInput v-model="state.street" class="w-full" />
+        </UFormField>
+    
+        <div class="flex gap-4">
+          <UFormField label="PLZ" name="zip" size="xl">
+            <UInput v-model="state.zip" class="w-full" />
+          </UFormField>
+      
+          <UFormField label="Ort" name="city" size="xl" class="grow">
+            <UInput v-model="state.city" class="w-full" />
+          </UFormField>
+        </div>
+    
+        <UFormField label="Telefon" name="phone" size="xl">
+          <UInput v-model="state.phone" class="w-full" />
+        </UFormField>
+    
+        <UFormField label="Email" name="email" size="xl">
+          <UInput v-model="state.email" class="w-full" />
+        </UFormField>
+    
+        <UButton :loading="isSaving" type="submit" color="primary" icon="i-heroicons-check" size="xl">
+          Speichern
+        </UButton>
+      </UForm>
     </div>
-
-    <UFormField label="Telefon" name="phone" size="xl">
-      <UInput v-model="state.phone" class="w-full" />
-    </UFormField>
-
-    <UFormField label="Email" name="email" size="xl">
-      <UInput v-model="state.email" class="w-full" />
-    </UFormField>
-
-    <UButton :loading="isSaving" type="submit" color="primary" icon="i-heroicons-check" size="xl">
-      Speichern
-    </UButton>
-  </UForm>
+    <div class="grow flex items-center justify-center">
+      <div class="border-dashed border-gray-300 border w-96">
+        <PrintVcard
+          :logo-width="120"
+          :title="state.title || 'Meine Firma'"
+          subtitle="Meine Firma"
+          slogan="Musterslogan"
+          description="Musterbeschreibung"
+          :street="state.street || 'Musterstraße 123'"
+          :zip="state.zip || '12345'"
+          :city="state.city || 'Musterstadt'"
+          :phone="state.phone || '0123456789'"
+          :email="state.email || 'kontakt@beispiel.com'"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
