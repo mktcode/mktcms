2f79df16aeb9d64000ddba8a619c6380d01765ef
mktcode <kontakt@markus-kottlaender.de>
2025-03-21 20:21:10 +0100

    feat: refactor Landingpage component and extract header into a new Header component

--- components/Landingpage.vue
+++ components/Landingpage.vue
@@ -7,13 +7,8 @@ const props = defineProps<{
   website: WebsiteWithContents
 }>()
 
-const router = useRouter()
-const contactLink = `${router.currentRoute.value.fullPath}#contact`
-
 const appConfig = useAppConfig()
 appConfig.ui.colors.primary = props.website.primaryColor || appConfig.ui.colors.primary
-
-const { public: { s3Endpoint } } = useRuntimeConfig()
 
 updateAppConfig(appConfig)
 </script>
@@ -30,29 +25,7 @@ updateAppConfig(appConfig)
     'font-playfair-display': website.font === 'playfair-display',
 
   }">
-    <div class="h-screen flex items-center justify-center bg-primary-950">
-      <img
-        v-if="website.image"
-        :src="`${s3Endpoint}/mktcms/${website.image}`"
-        class="absolute inset-0 object-cover w-full h-full opacity-10"
-      />
-      <div class="relative z-10 text-primary-50 flex flex-col p-6 sm:p-12 lg:p-24 items-start justify-center gap-8 max-w-7xl">
-        <div>
-          <h1 class="text-7xl font-bold mb-2">
-            {{ website.title }}
-          </h1>
-          <h2 class="text-4xl text-primary-300">
-            {{ website.subtitle }}
-          </h2>
-        </div>
-        <p class="text-2xl opacity-80">
-          {{ website.description }}
-        </p>
-        <UButton v-if="website.hasContactForm" size="xl" :to="contactLink">
-          Kontakt aufnehmen
-        </UButton>
-      </div>
-    </div>
+    <LandingpageHeader :website="website" />
     <LandingpageAbout v-if="website.showAbout" :website="website" />
     <LandingpageContents v-if="website.showContents" :website="website" />
     <LandingpageContactForm v-if="website.hasContactForm" :website="website" />
