bfacf3565742c19adfc1cc87d2718a801bbaff1e
mktcode <kontakt@markus-kottlaender.de>
2025-03-31 12:20:09 +0200

    feat: update authentication middleware to allow access to privacy policy page

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware((to) => {
   const { loggedIn } = useUserSession()
-  if (!loggedIn.value && to.path !== '/login' && to.path !== '/' && !to.path.startsWith('/start')) {
+  if (!loggedIn.value && to.path !== '/login' && to.path !== '/' && to.path !== '/datenschutz') {
     return navigateTo('/login')
   }
 })
