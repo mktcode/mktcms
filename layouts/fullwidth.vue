ea35f6ced3bedab736871f4176cf3222142de765
mktcode <kontakt@markus-kottlaender.de>
2025-04-04 10:33:08 +0200

    feat: add fullwidth layout for website live editing

---
+++ layouts/fullwidth.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const { loggedIn } = useUserSession();
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col font-nunito">
+    <LayoutNavbar v-if="loggedIn" />
+    <slot name="navbar2" />
+    
+    <main class="grow w-full">
+      <div class="bg-white border-x border-gray-200">
+        <slot />
+      </div>
+    </main>
+  </div>
+</template>
--- pages/werbung/website/[id].vue
+++ pages/werbung/website/[id].vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import type { Website, WebsiteContent } from '~/types';
+import type { Company, Website, WebsiteContent } from '~/types';
 
 type WebsiteWithContents = Website & { contents: WebsiteContent[] }
 
 const route = useRoute()
 const website = ref<WebsiteWithContents | null>(null);
 const websiteId = Array.isArray(route.params.id) ? Number(route.params.id[0]) : Number(route.params.id)
+const company = ref<Company | null>(null);
 
 definePageMeta({
   validate: async (route) => {
@@ -15,28 +16,37 @@ definePageMeta({
 
 onMounted(async () => {
   const existingWebsite = await $fetch<WebsiteWithContents>(`/api/websites/${websiteId}`);
+  const existingCompany = await $fetch(`/api/company`);
 
-  if (!existingWebsite) {
+  if (!existingWebsite || !existingCompany) {
     return;
   }
   
   website.value = existingWebsite;
+  company.value = existingCompany;
 })
 </script>
 
 <template>
-  <NuxtLayout name="default">
+  <NuxtLayout name="fullwidth">
     <template #navbar2>
       <LayoutNavbarAds />
     </template>
-    <div class="flex items-center gap-4 p-6">
-      <h1 class="text-2xl font-bold">
-        {{ website ? website.title : 'Lade...' }}
-      </h1>
-      <UButton v-if="website" :to="`/website/${website.id}`" target="_blank" icon="i-lucide-external-link" variant="ghost">
-        Website öffnen
-      </UButton>
+    <div class="flex flex-col md:flex-row">
+      <div class="md:w-72 2xl:w-80 flex-none">
+        <div class="flex flex-col gap-4 p-3">
+          <h1 class="text-xl font-bold">
+            {{ website ? website.title : 'Lade...' }}
+          </h1>
+          <UButton v-if="website" :to="`/website/${website.id}`" target="_blank" trailing-icon="i-lucide-external-link" variant="ghost">
+            Website öffnen
+          </UButton>
+        </div>
+        <WebsitesForm v-if="website" :website="website"/>
+      </div>
+      <div class="flex-1">
+        <Landingpage v-if="website && company" :website="website" :company="company" :is-live="false" />
+      </div>
     </div>
-    <WebsitesForm v-if="website" :website="website"/>
   </NuxtLayout>
 </template>
