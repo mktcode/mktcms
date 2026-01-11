bb4f5ff0edf95e7e3d016013e4c3d20885643e96
mktcode <kontakt@markus-kottlaender.de>
2026-01-11 19:27:01 +0100

    added auth middleware

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,4 @@
-import { defineNuxtModule, addServerPlugin, createResolver } from '@nuxt/kit'
+import { defineNuxtModule, addServerPlugin, createResolver, addServerHandler } from '@nuxt/kit'
 import defu from 'defu'
 
 export default defineNuxtModule({
@@ -9,8 +9,9 @@ export default defineNuxtModule({
   setup(_options, _nuxt) {
     const resolver = createResolver(import.meta.url)
 
-    // S3
+    // Runtime Config
     _nuxt.options.runtimeConfig.mktcms = defu((_nuxt.options.runtimeConfig.mktcms, {
+      adminAuthKey: '',
       s3AccessKey: '',
       s3SecretKey: '',
       s3Endpoint: '',
@@ -19,5 +20,11 @@ export default defineNuxtModule({
     }))
 
     addServerPlugin(resolver.resolve('./runtime/server/plugins/storage'))
+
+    // Auth Middleware
+    addServerHandler({
+      middleware: true,
+      handler: resolver.resolve('./runtime/server/middleware/auth')
+    })
   },
 })
---
+++ src/runtime/server/middleware/auth.ts
@@ -0,0 +1,24 @@
+import { useRuntimeConfig } from 'nitropack/runtime'
+import { createError, defineEventHandler, getCookie, getRequestURL, sendRedirect } from 'h3'
+
+export default defineEventHandler(async (event) => {
+  const pathname = getRequestURL(event).pathname
+
+  const isAdminLoginRoute = pathname === '/admin/login' || pathname === '/api/admin/login'
+  const isAdminRoute = pathname === '/admin' || pathname.startsWith('/admin/')
+  const isAdminApiRoute = pathname === '/api/admin' || pathname.startsWith('/api/admin/')
+  if (isAdminLoginRoute || (!isAdminRoute && !isAdminApiRoute)) return
+
+  const { mktcms: { adminAuthKey } } = useRuntimeConfig()
+
+  const authKeyCookie = getCookie(event, 'admin_auth_key')
+
+  if (!authKeyCookie || authKeyCookie !== adminAuthKey.toString() || adminAuthKey === '') {
+    if (isAdminApiRoute) {
+      throw createError({ statusCode: 401, statusMessage: 'Unauthorized' })
+    }
+    else {
+      return sendRedirect(event, '/admin/login')
+    }
+  }
+})
