6c6607211e9cd763a82514abd0b72f9dcb0fda7a
mktcode <kontakt@markus-kottlaender.de>
2026-01-19 21:10:09 +0100

    Refactor code structure for improved readability and maintainability

--- src/runtime/app/components/content/editor/image.vue
+++ src/runtime/app/components/content/editor/image.vue
@@ -5,10 +5,18 @@ const { path } = usePathParam();
 </script>
 
 <template>
-  <div class="image-editor">
+  <div>
+    <button class="button secondary w-full justify-center mb-2" type="button">
+      Link kopieren
+    </button>
+    <button class="button w-full justify-center mb-4" type="button">
+      Bild austauschen
+    </button>
+
     <img
       :src="`/api/admin/content/${path}`"
       alt="Image Preview"
+      class="w-full h-auto border border-gray-200 rounded-sm p-1"
     >
   </div>
 </template>
--- src/runtime/app/components/content/editor/pdf.vue
+++ src/runtime/app/components/content/editor/pdf.vue
@@ -6,10 +6,16 @@ const { path } = usePathParam();
 
 <template>
   <div>
+    <button class="button secondary w-full justify-center mb-2" type="button">
+      Link kopieren
+    </button>
+    <button class="button w-full justify-center mb-4" type="button">
+      PDF austauschen
+    </button>
     <embed
       :src="`/api/admin/content/${path}`"
       type="application/pdf"
-      class="w-full h-auto aspect-[5.5/8]"
+      class="w-full h-auto aspect-[5.5/8] border border-gray-200 rounded-sm"
     />
   </div>
 </template>
