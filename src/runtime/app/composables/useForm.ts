a48c6fd6d53120a91328dd2315156ece2bb63082
mktcode <kontakt@markus-kottlaender.de>
2026-01-22 21:09:54 +0100

    feat: enhance useForm composable with validation schema and error handling

--- src/runtime/app/composables/useForm.ts
+++ src/runtime/app/composables/useForm.ts
@@ -1,36 +1,70 @@
+import { z } from 'zod'
 import { ref } from 'vue'
 
-export function useForm(endpoint: string, successMessageText: string, errorMessageText: string) {
+export function useForm(
+  endpoint: string,
+  successMessageText: string,
+  errorMessageText: string,
+  validationSchema: z.ZodObject<any>
+) {
   const isSending = ref(false)
   const successMessage = ref('')
   const errorMessage = ref('')
+  const validationErrors = ref<Record<string, string[] | undefined>>({})
+  const fieldsTouched = ref<Record<string, boolean>>({})
 
-  async function sendForm(body: Record<string, any>) {
+  function validate(body: Record<string, any>) {
+    const parseResult = validationSchema.safeParse(body)
+    
+    if (parseResult.success) {
+      validationErrors.value = {}
+      return true
+    } else {
+      validationErrors.value = z.flattenError(parseResult.error).fieldErrors
+      return false
+    }
+  }
+
+  function validationErrorsFor(field: string) {
+    if (!fieldsTouched.value[field]) return []
+
+    return validationErrors.value[field] || []
+  }
+
+  async function send(body: Record<string, any>) {
     if (isSending.value) return
-    isSending.value = true
+    
     successMessage.value = ''
     errorMessage.value = ''
+
+    validate(body)
+
+    if (validationErrors.value) return
+
+    isSending.value = true
 
     try {
       await $fetch(endpoint, {
         method: 'POST',
         body,
       })
-
+  
       successMessage.value = successMessageText
-    }
-    catch {
+    } catch (error) {
       errorMessage.value = errorMessageText
     }
-    finally {
-      isSending.value = false
-    }
+
+    isSending.value = false
   }
 
   return {
     isSending,
     successMessage,
     errorMessage,
-    sendForm,
+    validationErrors,
+    fieldsTouched,
+    validationErrorsFor,
+    validate,
+    send,
   }
 }
