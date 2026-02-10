f05f0f2daf8c711e22202f10740fd6dbef53e6e2
mktcode <kontakt@markus-kottlaender.de>
2026-02-10 22:34:06 +0100

    feat: enhance sendMail function to support HTML and text templates using EJS

--- src/runtime/server/utils/sendMail.ts
+++ src/runtime/server/utils/sendMail.ts
@@ -1,7 +1,20 @@
 import nodemailer from 'nodemailer'
+import ejs from 'ejs';
 import { useRuntimeConfig } from 'nitropack/runtime'
 
-export async function sendMail({ subject, fields, replyTo }: { subject: string, fields: Record<string, any>, replyTo?: string }) {
+export async function sendMail({
+  subject,
+  fields,
+  replyTo,
+  templateHtml,
+  templateText,
+}: {
+  subject: string,
+  fields: Record<string, any>,
+  replyTo?: string,
+  templateHtml?: string,
+  templateText?: string,
+}) {
   const { mktcms: { smtpHost, smtpPort, smtpUser, smtpPass, smtpSecure, mailerFrom, mailerTo } } = useRuntimeConfig()
 
   const transporter = nodemailer.createTransport({
@@ -14,13 +27,16 @@ export async function sendMail({ subject, fields, replyTo }: { subject: string, 
     },
   })
 
+  const finalHtml = templateHtml ? ejs.render(templateHtml, { fields }) : Object.entries(fields).map(([key, value]) => `<p><strong>${key}:</strong> ${value}</p>`).join('')
+  const finalText = templateText ? ejs.render(templateText, { fields }) : Object.entries(fields).map(([key, value]) => `${key}: ${value}`).join('\n')
+
   const mailOptions = {
     from: mailerFrom,
     to: mailerTo,
     replyTo: replyTo || undefined,
     subject: subject,
-    html: Object.entries(fields).map(([key, value]) => `<p><strong>${key}:</strong> ${value}</p>`).join(''),
-    text: Object.entries(fields).map(([key, value]) => `${key}: ${value}`).join('\n'),
+    html: finalHtml,
+    text: finalText,
   }
 
   return await transporter.sendMail(mailOptions)
