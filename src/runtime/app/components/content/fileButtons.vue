f0b7f038341f852859fe16adf238f98b0b5e28e2
mktcode <kontakt@markus-kottlaender.de>
2026-03-04 12:33:38 +0100

    fix: update copy button functionality and improve UI consistency

--- src/runtime/app/components/content/fileButtons.vue
+++ src/runtime/app/components/content/fileButtons.vue
@@ -25,6 +25,27 @@ function copyEditRoute(path: string) {
 <template>
   <div class="flex gap-2">
     <NuxtLink
+      v-if="isCopyableTextFile(filePath)"
+      :to="copyEditRoute(filePath)"
+      class="button secondary"
+      title="Datei kopieren"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="size-4"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M8.25 7.5V6A2.25 2.25 0 0 1 10.5 3.75h7.5A2.25 2.25 0 0 1 20.25 6v7.5a2.25 2.25 0 0 1-2.25 2.25H16.5m-8.25-8.25h7.5A2.25 2.25 0 0 1 18 9.75v7.5a2.25 2.25 0 0 1-2.25 2.25h-7.5A2.25 2.25 0 0 1 6 17.25v-7.5A2.25 2.25 0 0 1 8.25 7.5Z"
+        />
+      </svg>
+    </NuxtLink>
+    <NuxtLink
       class="button secondary"
       title="Link kopieren"
       @click.prevent="copy(siteUrl + '/api/content/' + filePath)"
@@ -57,27 +78,6 @@ function copyEditRoute(path: string) {
           stroke-linecap="round"
           stroke-linejoin="round"
           d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
-        />
-      </svg>
-    </NuxtLink>
-    <NuxtLink
-      v-if="isCopyableTextFile(filePath)"
-      :to="copyEditRoute(filePath)"
-      class="button secondary"
-      title="Datei kopieren"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="size-4"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M8.25 7.5V6A2.25 2.25 0 0 1 10.5 3.75h7.5A2.25 2.25 0 0 1 20.25 6v7.5a2.25 2.25 0 0 1-2.25 2.25H16.5m-8.25-8.25h7.5A2.25 2.25 0 0 1 18 9.75v7.5a2.25 2.25 0 0 1-2.25 2.25h-7.5A2.25 2.25 0 0 1 6 17.25v-7.5A2.25 2.25 0 0 1 8.25 7.5Z"
         />
       </svg>
     </NuxtLink>
