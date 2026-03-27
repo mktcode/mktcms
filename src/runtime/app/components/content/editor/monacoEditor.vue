87ce0ef5cd9c2537c9cb70fed57b71c9bad1ac63
mktcode <kontakt@markus-kottlaender.de>
2026-03-27 19:39:11 +0100

    refactor(monacoEditor): simplify worker setup and restrict language support to markdown

--- src/runtime/app/components/content/editor/monacoEditor.vue
+++ src/runtime/app/components/content/editor/monacoEditor.vue
@@ -3,18 +3,15 @@ import { onBeforeUnmount, onMounted, ref, watch } from 'vue'
 import type * as Monaco from 'monaco-editor'
 
 import 'monaco-editor/min/vs/editor/editor.main.css'
+import 'monaco-editor/esm/vs/basic-languages/markdown/markdown.contribution'
 
-import * as monaco from 'monaco-editor'
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js'
 
 import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
-import JsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
-import CssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
-import HtmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
-import TsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 
 const props = withDefaults(defineProps<{
   modelValue: string
-  language?: string
+  language?: 'markdown'
   readOnly?: boolean
 }>(), {
   language: 'markdown',
@@ -38,16 +35,7 @@ function ensureMonacoWorkers() {
     return
 
   globalAny.MonacoEnvironment = {
-    getWorker(_: unknown, label: string) {
-      if (label === 'json')
-        return new JsonWorker()
-      if (label === 'css' || label === 'scss' || label === 'less')
-        return new CssWorker()
-      if (label === 'html' || label === 'handlebars' || label === 'razor')
-        return new HtmlWorker()
-      if (label === 'typescript' || label === 'javascript')
-        return new TsWorker()
-
+    getWorker() {
       return new EditorWorker()
     },
   }
